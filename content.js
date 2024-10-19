5b4ac1ae5c784780c9c95b625819ce74f21ccfe8
Ross Dooney <rossdooney@gmail.com>
2024-10-19 18:41:48 +0100

    added listner to tab

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
 console.log("content script running");
+
+chrome.runtime.onMessage.addListener((messgae, sender, sendResponse) => {
+    if(messgae.action === "recording_request"){
+        console.log("recording requested");
+        sendResponse("seen");
+    }
+
+})
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     const startVideoButton = document.querySelector("button#start_video")
     const stopVideoButton = document.querySelector("button#stop_video")
+
+    startVideoButton.addEventListener("click", ()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "recording_request"}, function(response){
+                if(!chrome.runtime.lastError){
+                    console.log(response)
+                }else{
+                    console.log(chrome.runtime.lastError, 'line 11 error')
+                }
+            })
+        })
+    })
+
+    stopVideoButton.addEventListener("click", ()=>{
+        console.log("stop button clicked")
+    })
 })
