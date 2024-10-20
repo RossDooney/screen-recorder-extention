5d859027ae7a673219be0f400b443b8425aab48a
Ross Dooney <rossdooney@gmail.com>
2024-10-20 12:50:13 +0100

    screen recorder completed and auto file download

--- content.js
+++ content.js
@@ -1,9 +1,65 @@
 console.log("content script running");
 
-chrome.runtime.onMessage.addListener((messgae, sender, sendResponse) => {
-    if(messgae.action === "recording_request"){
+var recorder = null
+
+function onAccessApproved(stream){
+    recorder = new MediaRecorder(stream)
+
+    recorder.start();
+
+    recorder.onstop = function(){
+        stream.getTracks().forEach(function(track){
+            if (track.readyState === "live"){
+                track.stop();
+            }
+        })
+    }
+
+    recorder.ondataavailable = function(event){
+        let recordedData = event.data;
+        let url = URL.createObjectURL(recordedData);
+
+        let a = document.createElement("a");
+
+        a.style.display = "none";
+        a.href = url;
+        a.download = "screen-recording.webm";
+
+        document.body.append(a);
+        a.click();
+
+        document.body.removeChild(a);
+
+        URL.revokeObjectURL(url);
+    }
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if(message.action === "recording_request"){
         console.log("recording requested");
-        sendResponse("seen");
+        sendResponse(`seen: ${message.action}`);
+
+        navigator.mediaDevices.getDisplayMedia({
+            audio: true,
+            video:{
+                width: 999999999,
+                height: 999999999
+            }
+        }).then((stream)=>{
+            onAccessApproved(stream)
+        })
+    }
+    
+    if (message.action === "stop_recording"){
+        console.log("Stop recording");
+        sendResponse(`seen: ${message.action}`);
+        
+        if(!recorder){
+            return console.log("no recording")
+        }
+        else{
+            recorder.stop();
+        }
     }
 
 })
--- js/popup.js
+++ js/popup.js
@@ -15,6 +15,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
     })
 
     stopVideoButton.addEventListener("click", ()=>{
-        console.log("stop button clicked")
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "stop_recording"}, function(response){
+                if(!chrome.runtime.lastError){
+                    console.log(response)
+                }else{
+                    console.log(chrome.runtime.lastError, 'line 23 error')
+                }
+            })
+        })
     })
 })
